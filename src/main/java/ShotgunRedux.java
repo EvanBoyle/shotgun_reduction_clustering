7dbcd46e2091235279ffa726c323369e412e0ad4
EvanBoyle <epb5te@virginia.edu>
2014-01-07 17:25:00 -0500

    init commit, created maven project

---
+++ src/main/java/ShotgunRedux.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: boyleep
+ * Date: 1/7/14
+ * Time: 5:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShotgunRedux {
+}
